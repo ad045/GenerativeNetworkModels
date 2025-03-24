93fbc81a6d13135c4ed687701b209c1b3bc8039b
will <will.b.mills@outlook.com>
2025-03-24 12:16:52 +0000

    search by variable names fully implemented + test script

--- src/gnm/fitting/experiment_saving.py
+++ src/gnm/fitting/experiment_saving.py
@@ -15,6 +15,7 @@ from .experiment_dataclasses import (
     WeightedSweepParameters
 )
 import os
+import torch
 
 class ExperimentEvaluation():
     def __init__(self, path=None, index_file_path=None, variables_to_ignore=[]):
@@ -30,13 +31,14 @@ class ExperimentEvaluation():
 
         self.path = path
         self.index_path = os.path.join(self.path, index_file_path)      
-        self._refresh_index_file()
 
         # get the variables we want to save, i.e. alpha, gamma etc (some will be in list format)
         binary_variables_to_save = [f.name for f in fields(BinarySweepParameters)]
         weighted_variables_to_save = [f.name for f in fields(WeightedSweepParameters)]
         variables_to_save = binary_variables_to_save + weighted_variables_to_save
         self.variables_to_save = [i for i in variables_to_save if i not in variables_to_ignore]
+
+        self._refresh_index_file()
 
     def _refresh_index_file(self):
         if not os.path.exists(self.index_path):
@@ -50,12 +52,11 @@ class ExperimentEvaluation():
     def _make_index_file(self):
         date = datetime.now()
         date_formatted = date.strftime("%d/%m/%Y")
-        all_variables_to_save = self.binary_variables_to_save + self.weighted_variables_to_save
         json_initial_data = {
             'date':date_formatted,
             'experiment_configs':{
                 'test_config':{
-                    i:'TEST' for i in all_variables_to_save
+                    i:'TEST' for i in self.variables_to_save
                 }
             }
         }
@@ -65,7 +66,11 @@ class ExperimentEvaluation():
 
         self._refresh_index_file()
 
-    def save_experiment(self, experiment_dataclass:Experiment, experiment_name='gnm_experiment'):
+    def save_experiments(self, experiments:list[Experiment]):
+        for experiment in experiments:
+            self._save_experiment(experiment)
+
+    def _save_experiment(self, experiment_dataclass:Experiment, experiment_name='gnm_experiment'):
 
         def strip_string_to_int(string_value):
             final_number = ''
@@ -101,9 +106,24 @@ class ExperimentEvaluation():
         # may ignore weighted parameters if set to None
         all_config = asdict(experiment_dataclass.run_config.binary_parameters)
         all_config.update(asdict(experiment_dataclass.run_config.weighted_parameters))
-        all_config = {key:repr(value) for key, value in all_config.items() if key in self.variables_to_save}
+    
+        # de-tensor floating and int values
+        formatted_config = {}
+        for key, value in all_config.items():
+            if isinstance(value, torch.Tensor):
+                formatted_config[key] = value.item()
+            elif not isinstance(value, (str, int, float, list)) and hasattr(value, "__class__"):
+                try:
+                    class_name = value.__class__.__name__
+                    formatted_config[key] = class_name
+                except:
+                    warn(f'Attribute {value} could not be saved - no name or class instance found.')
+            elif isinstance(value, list):
+                formatted_config[key] = repr(value)
+            elif isinstance(value, (int, float, str)):
+                formatted_config[key] = value
 
-        self.index_file['experiment_configs'][experiment_name] = all_config
+        self.index_file['experiment_configs'][experiment_name] = formatted_config
         
         # overwrite previous file
         with open(os.path.join(self.path, "gnm_index.json"), "w") as f:
@@ -117,20 +137,44 @@ class ExperimentEvaluation():
         pass
 
     def _sort_experiments(self, experiments, variable_to_sort_by, get_names_only=False):
+
+        def combine_dictionary_by_key(list_of_dictionaries, key_value_items):
+            exp = {}
+            for dictionary in list_of_dictionaries:
+                for name in list(dictionary.keys()):
+                    exp[name] = key_value_items[name][variable_to_sort_by]
+
+            return exp
+
         experiment_names = list(experiments.keys())
 
         # keys = experiment name, values = experiment values of the given variable to sort by
         sorting_dict = {experiment_name:value for experiment_name, value in zip(experiment_names, [experiments[name][variable_to_sort_by] for name in experiment_names])}
+        
+        # # convert strings back to original 
+        # sorting_dict = {experiment_name:eval(value) for experiment_name, value in sorting_dict.items()}
 
-        # sort dictionary by values
-        sorting_dict = dict(sorted(sorting_dict.items(), key=lambda item: item[1]))
+        # iterate to check types
+        sorting_dict_numbers = {}
+        sorting_dict_strings = {}
+        sroting_dict_lists = {}
+        for key, value in sorting_dict.items():
+            if isinstance(value, int) or isinstance(value, float):
+                sorting_dict_numbers[key] = value
+            elif isinstance(value, str):
+                sorting_dict_strings[key] = value
+            else:
+                sroting_dict_lists[key] = value
+
+        
+        # sort num, string dictionaries by values (sorted requires same datatype) - no point in sorting lists
+        sorting_dict_numbers = dict(sorted(sorting_dict_numbers.items(), key=lambda item: item[1]))
+        sorting_dict_strings = dict(sorted(sorting_dict_strings.items(), key=lambda item: item[1]))
 
         # create a new dictonary and add experiments based on the order of sorted_experiments
         # but with all the data included in experiments, rather than just the value used for sorting
-        sorted_experiments = {}
-        for name in list(sorting_dict.keys()):
-            sorted_experiments[name] = experiments[name][variable_to_sort_by]
-
+        sorted_experiments = combine_dictionary_by_key([sorting_dict_numbers, sorting_dict_strings, sroting_dict_lists], experiments)
+        
         if get_names_only:
             sorted_experiments = list(sorted_experiments.keys())
         
@@ -173,6 +217,22 @@ class ExperimentEvaluation():
     def purge_index_file(self):
         pass
 
+    def _is_similar_wording(self, variable_word, verbose=True):
+        all_vars = self.variables_to_save
+
+        char_frequency = {}
+        for var in all_vars:
+            letters_in_common = [character for character in variable_word if character in var]
+            char_frequency[var] = len(letters_in_common) / len(var)
+
+        char_frequency = dict(sorted(char_frequency.items(), key=lambda item: item[1]))
+        most_likely_word = list(char_frequency.keys())[-1]
+
+        if verbose:
+            print(f'Did you mean {most_likely_word}?')
+        
+        return most_likely_word
+
     # find experiment given a value of a variable like alpha, 
     # returns the experiment(s) of that value 
     def query_experiments(self, value=None, by=None, limit=float('inf'), verbose=True):
@@ -187,7 +247,9 @@ class ExperimentEvaluation():
         searchable_variables = list(first_experiment_data.keys())
 
         # make sure variable provided can be searched
-        assert by in searchable_variables, f'Variable {by} not in searchable variables. Must be one of {searchable_variables}'
+        if by not in searchable_variables:
+            print(f'Variable {by} not in searchable variables. Must be one of {searchable_variables}')
+            self._is_similar_wording()
 
         # sort by that variable and return list if no value to search for is specified
         if value is None or len(all_experiments) == 1:
